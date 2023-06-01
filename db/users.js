8c4f4f6f133f8181dfef9ef4a4d18fa134d86029
Derek Nguyen <derek.nguyen02@gmail.com>
2023-05-31 20:36:27 -0700

    working on createUser

--- db/users.js
+++ db/users.js
@@ -1,26 +1,35 @@
 const client = require("./client");
+const bcrypt = require("bcrypt");
 
 // database functions
 
 // user functions
 async function createUser({ username, password }) {
   
-  // const hashedPassword = //need to add hash function here
+  const SALT_COUNT = 10;
+
+  const hashedPassword = await bcrypt.hash(password, SALT_COUNT)
   
-  // try {
-  //   const { rows: [user] } = client.query(`
-  //     INSERT INTO users(username, password)
-  //     VALUES($1,$2)
-  //     ON CONFLICT (username) DO NOTHING
-  //     RETURNING *;
-  //   `,[username, hashedPassword])
-  // } catch (err) {
-  //   throw err;
-  // }
+  try {
+    const { rows: [user] } = client.query(`
+      INSERT INTO users(username, password)
+      VALUES($1,$2)
+      ON CONFLICT (username) DO NOTHING
+      RETURNING *;
+    `,[username, hashedPassword])
+
+    return user;
+  } catch (err) {
+    throw err;
+  }
+
 }
 
 async function getUser({ username, password }) {
+  // const user = await getUserByUsername(username);
+  // const hashedPassword = user.password;
 
+  // const isValid = await bcrypt.compare(password, hashedPassword);
 }
 
 async function getUserById(userId) {
