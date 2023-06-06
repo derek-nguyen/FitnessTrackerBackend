828dfc3a162fee4528715e0bd9685ce17b879002
Raymond Nhan <nhanraymond@gmail.com>
2023-06-05 19:08:43 -0700

    wokring on server responsive

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
+
+const server = express();
+const apiRouter = require("./api");
 
 // Setup your Middleware and API Router here
+server.use(cors());
+server.use(morgan("dev"));
+server.use(express.json());
+server.use("/api", apiRouter);
+
+server.use((req, res, next) => {
+  res.status(404).send("Page not found");
+});
+
+server.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: err });
+});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 const http = require("http");
 const chalk = require("chalk");
 const app = require("./app");
+const client = require("./db/client");
 
 const PORT = process.env["PORT"] ?? 3000;
 const server = http.createServer(app);
 
-//todo not able to use server.use
-/*
-const apiRouter = require("./api");
-server.use("/api", apiRouter);
-*/
-
-//todo should we do this instead? but server is already being used
-/*
-const express = require('express');
-const server = express();
-*/
 server.listen(PORT, () => {
+  client.connect();
   console.log(
     chalk.blueBright("Server is listening on PORT:"),
     chalk.yellow(PORT),
