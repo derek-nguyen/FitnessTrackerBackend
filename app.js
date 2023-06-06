dc0e870fbc4c2f5d9b296400fe113e9537c7da51
Raymond Nhan <nhanraymond@gmail.com>
2023-06-05 19:38:11 -0700

    server working now; accidentally used express() twice

--- app.js
+++ app.js
@@ -4,20 +4,19 @@ const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
 
-const server = express();
 const apiRouter = require("./api");
 
 // Setup your Middleware and API Router here
-server.use(cors());
-server.use(morgan("dev"));
-server.use(express.json());
-server.use("/api", apiRouter);
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/api", apiRouter);
 
-server.use((req, res, next) => {
+app.use((req, res, next) => {
   res.status(404).send("Page not found");
 });
 
-server.use((err, req, res, next) => {
+app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).json({ error: err });
 });
