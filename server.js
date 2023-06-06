e5cfe5d8d8960397676fec45f14c8dde0c09d332
Raymond Nhan <nhanraymond@gmail.com>
2023-06-05 17:34:05 -0700

    need clarification on server.js

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ const app = require("./app");
 const PORT = process.env["PORT"] ?? 3000;
 const server = http.createServer(app);
 
+//todo not able to use server.use
+/*
 const apiRouter = require("./api");
 server.use("/api", apiRouter);
+*/
 
+//todo should we do this instead? but server is already being used
+/*
+const express = require('express');
+const server = express();
+*/
 server.listen(PORT, () => {
   console.log(
     chalk.blueBright("Server is listening on PORT:"),
